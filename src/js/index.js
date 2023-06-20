0828c99ebb8a5f04337f00c4f1125a50cebc47b3
jiho <hahhaah@naver.com>
2023-06-20 09:45:08 +0900

    step1 완료

--- src/js/index.js
+++ src/js/index.js
@@ -6,31 +6,6 @@ function App() {
     $(".menu-count").innerText = `총 ${menuCount}개`;
   };
 
-  $("#espresso-menu-list").addEventListener("click", (e) => {
-    const $menuName = e.target.closest("li").querySelector(".menu-name");
-
-    if (e.target.classList.contains("menu-edit-button")) {
-      const updatedMenuName = prompt(
-        "메뉴명을 수정하세요",
-        $menuName.innerText
-      );
-      $menuName.innerText = updatedMenuName;
-    }
-
-    if (e.target.classList.contains("menu-remove-button")) {
-      const remove = confirm(`${$menuName.innerText}를 삭제하시겠습니까?`);
-      if (remove) {
-        e.target.closest("li").remove();
-        alert(`${$menuName.innerText}를 삭제했습니다.`);
-        updateMenuCount();
-      }
-    }
-  });
-
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
-
   const addMenuName = () => {
     const espressoMenuName = $("#espresso-menu-name").value;
 
@@ -66,15 +41,41 @@ function App() {
 
     $("#espresso-menu-name").value = "";
 
-    // const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-    // $(".menu-count").innerText = `총 ${menuCount}개`;
     updateMenuCount();
   };
 
-  //메뉴 입력 후 확인 버튼
-  $("#espresso-menu-submit-button").addEventListener("click", () => {
-    addMenuName();
+  const updateMenuName = (e) => {
+    const $menuName = e.target.closest("li").querySelector(".menu-name");
+    const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+    $menuName.innerText = updatedMenuName;
+  };
+
+  const removeMenuName = (e) => {
+    const $menuName = e.target.closest("li").querySelector(".menu-name");
+    const remove = confirm(`${$menuName.innerText}를 삭제하시겠습니까?`);
+    if (remove) {
+      e.target.closest("li").remove();
+      alert(`${$menuName.innerText}를 삭제했습니다.`);
+      updateMenuCount();
+    }
+  };
+
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      updateMenuName(e);
+    }
+
+    if (e.target.classList.contains("menu-remove-button")) {
+      removeMenuName(e);
+    }
   });
+
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  //메뉴 입력 후 확인 버튼
+  $("#espresso-menu-submit-button").addEventListener("click", addMenuName);
 
   //메뉴 입력 후 엔터
   $("#espresso-menu-name").addEventListener("keypress", (e) => {
